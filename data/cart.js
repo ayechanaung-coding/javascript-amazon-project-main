2c0a5a789bccd525c145120f3cb80dd4aca45a50
Aye Chan Aung <ayechanaung@ucsy.edu.mm>
2024-06-27 13:53:52 +0630

    generate HTML for payment Summary

--- data/cart.js
+++ data/cart.js
@@ -4,8 +4,8 @@ if(!carts){
 
     carts=[{
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-        deliveryOptionId:'1'
+        quantity: 5,
+        deliveryOptionId:'3'
       },
       {
         productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
