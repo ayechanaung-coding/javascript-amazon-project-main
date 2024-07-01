800cdcd968a3d20bbf4baf7f5b6bf9627dddf67f
Aye Chan Aung <ayechanaung@ucsy.edu.mm>
2024-07-01 15:47:26 +0630

    seperate function calculateDeliveryDate

--- script/checkout/orderSummary.js
+++ script/checkout/orderSummary.js
@@ -3,7 +3,7 @@ import {products,getProduct} from '../../data/products.js'
 import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';import { renderPaymentSummary } from './paymentSummary.js';
+import {deliveryOptions, getDeliveryOption, calculateDeliveryDate} from '../../data/deliveryOptions.js';
 //import { renderPaymentSummary } from './paymentSummary.js';
 import { renderCheckoutHeader } from './checkoutHearder.js';
 
@@ -21,11 +21,8 @@ export function renderOrderSummary(){
       
       const deliveryOption=getDeliveryOption(deliveryOptionId);
      
-  
-    const today=dayjs();
-    const deliveryDate=today.add(deliveryOption.deliveryDays,'days');
-    const dateString=deliveryDate.format('dddd, MMMM D');
-  
+      const dateString=calculateDeliveryDate(deliveryOption);
+
     cartSummaryHtml+= `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery Date: ${dateString}
@@ -70,9 +67,7 @@ export function renderOrderSummary(){
   function deliveryOptionsHTML(matchingProduct,cartItem){
     let deliveryHTML='';
      deliveryOptions.forEach((deliveryOption)=>{
-      const today=dayjs();
-      const deliveryDate=today.add(deliveryOption.deliveryDays,'days');
-      const dateString=deliveryDate.format('dddd, MMMM D');
+      const dateString=calculateDeliveryDate(deliveryOption);
       const priceString=deliveryOption.priceCents===0?'FREE':`$${formatCurrency(deliveryOption.priceCents)} -`;
       const isChecked=deliveryOption.id===cartItem.deliveryOptionId?'checked':'';
       deliveryHTML+=
